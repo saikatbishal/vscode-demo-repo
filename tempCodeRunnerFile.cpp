03b18b9e7559e7faa1126ad08da87754c5f45faa
Saikat Bishal <2017ugmm037@nitjsr.ac.in>
2020-10-07 18:39:10 +0530

    new files

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,18 +1,40 @@
 #include <iostream>
 using namespace std;
 #include <vector>
+#include <algorithm>
 /* begin() end() rbegin() rend() cbegin() crbegin() crend() */
+bool f(int x,int y)
+{
+  return x > y;
+}
 int main()
 {
-  vector<int> g1;
-  for (int i = 1; i < 5;i++)
-  {
-    g1.push_back(i);
-  }
-  cout << "Output of begin and end :";
-  for (auto i = g1.begin(); i != g1.end() ; ++i)
-  {
-    cout << *i << " ";
-    cout << "This is the implementation of vectors. ";
-  }
+  vector<int> s = {11, 2, 3, 14, 51, 6};
+  cout << s[1]<<" "<<s[3]<<endl;
+  sort(s.begin(), s.end());//O(N logN)
+  bool present = binary_search(s.begin(), s.end(), 3);
+  cout << present;
+  present = binary_search(s.begin(), s.end(), 33);
+  cout << endl
+      << present;
+  s.push_back(100);
+  cout << endl
+      << binary_search(s.begin(), s.end(), 100) << endl;
+  
+  s.push_back(100); 
+  s.push_back(100); 
+  s.push_back(100); 
+  s.push_back(100); 
+ // s.push_back(100);
+  s.push_back(132);
+  vector<int>::iterator it = lower_bound(s.begin(), s.end(), 100);
+  vector<int>::iterator it2 = upper_bound(s.begin(), s.end(), 100);
+  cout << *it << " " << *it2 << endl;
+  cout << it2 - it << endl;
+  sort(s.begin(), s.end(), f);
+  vector<int>::iterator it3;
+  for (it3 = s.begin(); it3 != s.end(); ++it3)
+{
+  cout << *it3 << " ";
+}
 }
